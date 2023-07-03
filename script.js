ae4c60545d8751e46eb3d34b14128e938c65e7ce
abhi-rout <130905508+abhi-rout@users.noreply.github.com>
2023-07-03 23:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+ // var divElement = document.createElement('div');
+ //        divElement.setAttribute('id', 'abc');
+ //        document.body.appendChild(divElement);
+ var browserName = navigator.appName;
+var version = navigator.appVersion;
+var browserInfoDiv = document.getElementById('browser-info');
+browserInfoDiv.textContent = "You are using " + browserName + " version " + version;
